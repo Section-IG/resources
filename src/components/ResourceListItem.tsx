6a3c10e065b08849ad8625129dbbdb6a84c3fd4b
HunteRoi <tinael.devresse@hotmail.com>
2020-10-10 19:09:39 +0200

    Change card header to card title & remove uppercase

--- src/components/ResourceListItem.tsx
+++ src/components/ResourceListItem.tsx
@@ -5,11 +5,10 @@ import { ReactTinyLink } from 'react-tiny-link';
 
 const ResourceListItem: React.FC<ResourceModel> = ({ id, url, title, description, tags}) => { 
   return (
-    <Card className='text-center' key={id}>
-      { title && <Card.Header>{title.toUpperCase()}</Card.Header> }
-      
+    <Card className='text-center' key={id}>      
       <Card.Body>
         <ReactTinyLink showGraphic={true} url={url} cardSize='small' proxyUrl='https://cors.team-radiateur.fun:6443' />
+        { title && <Card.Title>{title}</Card.Title> }
         { description && <Card.Text className='mt-2'>{description}</Card.Text> }
         <Button className='mt-2' href={url} target='_blank' variant="secondary">Open link</Button>
       </Card.Body>
