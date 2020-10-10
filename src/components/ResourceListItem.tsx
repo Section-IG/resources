d05386632e9ad9d9b6e317ccd6ea1517d32ab2bd
HunteRoi <tinael.devresse@hotmail.com>
2020-10-10 18:51:12 +0200

    Update proxy url with new https proxy
    In order to avoid limitations of https://cors-anywhere.herokuapp.com, change proxyUrl property of ReactTinyLink component to use a custom one

--- src/components/ResourceListItem.tsx
+++ src/components/ResourceListItem.tsx
@@ -9,7 +9,7 @@ const ResourceListItem: React.FC<ResourceModel> = ({ id, url, title, description
       { title && <Card.Header>{title}</Card.Header> }
       
       <Card.Body>
-        <ReactTinyLink showGraphic={true} url={url} cardSize='small' />
+        <ReactTinyLink showGraphic={true} url={url} cardSize='small' proxyUrl='https://cors.team-radiateur.fun:6443' />
         { description && <Card.Text className='mt-2'>{description}</Card.Text> }
         <Button className='mt-2' href={url} target='_blank' variant="secondary">Open link</Button>
       </Card.Body>
